4d65de1ce1161964eecdb9d3c0f4f073e80c9f81
aflyafay <107936076+aflyafay@users.noreply.github.com>
2025-07-25 19:57:15 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -1004,7 +1004,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://aflyafay.x3322.net:7000".to_owned()
 }
 
 #[inline]
